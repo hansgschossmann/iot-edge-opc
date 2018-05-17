cfc41ae9d459fdf86c21c66553dfc2110056fff8
Hans Gschossmann <johanng@microsoft.com>
2018-05-17 21:47:40 +0200

    Fix port expose.

--- iotedgeopc-install-linux-packages.sh
+++ iotedgeopc-install-linux-packages.sh
@@ -14,25 +14,25 @@ echo "install utilities"
 apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 
 echo "install docker"
-apt-get remove docker docker-engine docker.io
+apt-get remove docker docker-engine docker.io -y
 apt-get install docker-ce -y
 
 echo "install docker-compose"
 #apt-get install docker-compose -y
-apt-get remove docker-compose
+apt-get remove docker-compose -y
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 echo "install azure cli"
-apt-get remove azure-cli
+apt-get remove azure-cli -y
 apt-get install azure-cli -y
 
 echo "install iot extension"
 az extension add --name azure-cli-iot-ext
 
 echo "install python and pip"
-apt-get remove python
-apt-get remove python-pip
+apt-get remove python -y
+apt-get remove python-pip -y
 apt-get install python -y
 apt-get install python-pip -y
 
